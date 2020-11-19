7d6f021db5a562fe5bc8064a6ab15b3020b66cbd
Ivan Stefanov <ivanjulianov@gmail.com>
2020-11-19 15:23:18 +0200

    package.json changes for deployment on Heroku

--- src/App.js
+++ src/App.js
@@ -4,5 +4,5 @@ const app = express();
 const router = require('./routes/router');
 app.use('/', router);
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log('Server running on port ' + PORT));
